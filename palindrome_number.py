b7b86c685e59ad28f5e22d28ecb087c14985fb5d
mrodrigues17 <mrod1791@gmail.com>
2022-06-11 13:51:33 -0500

    Add solution to palindrome number #1

---
+++ palindrome_number.py
@@ -0,0 +1,21 @@
+# Using modulo to solve
+def palindrome_number(x):
+    # store the digits in a list
+    lst = []
+    while x > 0:
+        lst.append(x % 10)
+        x = x // 10
+    # Determine how many iterations to loop through list from both sides
+    iter_num = len(lst) // 2
+    palindrome_counter = 0
+    for i in range(iter_num):
+        if lst[i] == lst[-i -1]:
+            pass
+        elif lst[i] != lst[-i -1]:
+            palindrome_counter +=1
+            return False
+    return True
+    
+
+
+print(palindrome_number(1234543211))
